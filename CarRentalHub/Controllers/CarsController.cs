6df670937cf5367a25e5d8a43ab1baf56d345a1d
bugaj55113 <bbugi2016@gmail.com>
2023-12-27 14:46:37 +0100

    Price column fixed

--- CarRentalHub/Controllers/CarsController.cs
+++ CarRentalHub/Controllers/CarsController.cs
@@ -55,7 +55,7 @@ namespace CarRentalHub.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,VehicleBrand,CarModel,Generation,BodyType,YearOfProduction,FuelType,Mileage")] Car car)
+        public async Task<IActionResult> Create([Bind("ID,VehicleBrand,CarModel,Generation,BodyType,YearOfProduction,FuelType,Mileage,Price")] Car car)
         {
             if (ModelState.IsValid)
             {
@@ -90,7 +90,7 @@ namespace CarRentalHub.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,VehicleBrand,CarModel,Generation,BodyType,YearOfProduction,FuelType,Mileage")] Car car)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,VehicleBrand,CarModel,Generation,BodyType,YearOfProduction,FuelType,Mileage,Price")] Car car)
         {
             if (id != car.ID)
             {
