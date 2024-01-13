dcd73a41be5ba2d23080969b9a73a1e4867baa75
bugaj55113 <bbugi2016@gmail.com>
2024-01-13 18:17:33 +0100

    Design corrected

--- CarRentalHub/Controllers/CarsController.cs
+++ CarRentalHub/Controllers/CarsController.cs
@@ -22,15 +22,17 @@ namespace CarRentalHub.Controllers
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly PhotoContext _photoContext;
         private readonly CarAvailabilityContext _carAvailabilityContext;
+        private readonly FilterDataContext _filterDataContext;
 
 
-        public CarsController(CarRentalHubContext context, UserManager<ApplicationUser> userManager, IWebHostEnvironment webHostEnvironment, PhotoContext photoContext, CarAvailabilityContext carAvailabilityContext)
+        public CarsController(CarRentalHubContext context, UserManager<ApplicationUser> userManager, IWebHostEnvironment webHostEnvironment, PhotoContext photoContext, CarAvailabilityContext carAvailabilityContext, FilterDataContext filterDataContext)
         {
             _context = context;
             _userManager = userManager;
             _webHostEnvironment = webHostEnvironment;
             _photoContext = photoContext;
             _carAvailabilityContext = carAvailabilityContext;
+            _filterDataContext = filterDataContext;
         }
 
         // GET: Cars
@@ -47,6 +49,48 @@ namespace CarRentalHub.Controllers
                     AdvertisementId = p.AdvertisementId
                 })
                 .ToList();
+
+            // Przekazanie parametrów formularza do widoku
+            ViewBag.FilterParameters = new FilterParameters
+            {
+                SelectedMarka = selectedMarka,
+                SelectedModel = selectedModel,
+                SelectedGeneration = selectedGeneration,
+                SelectedYearFrom = selectedYearFrom,
+                SelectedYearTo = selectedYearTo,
+                SelectedBodyType = selectedBodyType,
+                SelectedFuelType = selectedFuelType,
+                MileageFrom = MileageFrom,
+                MileageTo = MileageTo,
+                PriceFrom = PriceFrom,
+                PriceTo = PriceTo
+
+            };
+
+            ViewBag.DostepneMarki = PobierzListeMarek();
+
+            // Uzyskaj pierwszą markę z listy
+            var pierwszaMarka = (ViewBag.DostepneMarki as List<SelectListItem>)?.FirstOrDefault()?.Value;
+
+            // Pobierz dostępne modele dla pierwszej marki
+            ViewBag.DostepneModele = PobierzModeleZBazy(pierwszaMarka);
+
+            // Generacje
+            var firstCarModel = (ViewBag.DostepneModele as List<SelectListItem>)?.FirstOrDefault()?.Value;
+            ViewBag.AvailableGenerations = GetGenerationFromDatabase(firstCarModel);
+
+            // Lata
+            var years = Enumerable.Range(1900, DateTime.Now.Year - 1899).OrderByDescending(y => y).ToList();
+            ViewBag.AvailableYears = years;
+
+            // Typ nadwozia
+            List<string> bodyType = ["Auta małe", "Auta miejskie", "Coupe", "Kabriolet", "Kombi", "Kompakt", "Minivan", "Sedan", "Suv"];
+            ViewBag.BodyType = bodyType;
+
+            // Typ paliwa
+            List<string> fuelType = ["Benzyna", "Benzyna+CNG", "Benzyna+LPG", "Diesel", "Elektryczny", "Etanol", "Hybryda", "Wodór"];
+            ViewBag.FuelType = fuelType;
+
 
             if (!filteredCars.Any())
             {
@@ -468,5 +512,72 @@ namespace CarRentalHub.Controllers
         {
             return _context.CarInfoModel.Any(e => e.ID == id);
         }
+
+        private List<SelectListItem> PobierzListeMarek()
+        {
+            // Pobierz unikalne marki samochodów z bazy danych
+            var marki = _filterDataContext.FilterData
+                .Select(m => m.VehicleBrand)
+                .Distinct()
+                .ToList();
+
+            // Przygotuj listę SelectListItem dla rozwijanej listy
+            var listaMarek = marki.Select(m => new SelectListItem
+            {
+                Value = m,
+                Text = m
+            }).ToList();
+
+            return listaMarek;
+        }
+
+        [HttpGet]
+        public JsonResult PobierzModele(string marka)
+        {
+            // Tutaj pobierz modele samochodów dla danej marki z bazy danych
+            var modele = PobierzModeleZBazy(marka);
+
+            // Zwróć dane w formacie JSON
+            return Json(modele);
+        }
+
+        private List<string> PobierzModeleZBazy(string marka)
+        {
+            return _filterDataContext.FilterData
+                .Where(m => m.VehicleBrand == marka)
+                .Select(m => m.CarModel)
+                .Distinct()
+                .ToList();
+        }
+
+        public JsonResult GetGeneration(string carModel)
+        {
+            var generation = GetGenerationFromDatabase(carModel);
+            return Json(generation);
+        }
+
+        private List<string> GetGenerationFromDatabase(string carModel)
+        {
+            return _filterDataContext.FilterData
+                .Where(m => m.CarModel == carModel)
+                .Select(m => m.Generation)
+                .Distinct()
+                .ToList();
+        }
+    }
+
+    public class FilterParameters
+    {
+        public string SelectedMarka { get; set; }
+        public string SelectedModel { get; set; }
+        public string SelectedGeneration { get; set; }
+        public int? SelectedYearFrom { get; set; }
+        public int? SelectedYearTo { get; set; }
+        public string SelectedBodyType { get; set; }
+        public string SelectedFuelType { get; set; }
+        public int? MileageFrom { get; set; }
+        public int? MileageTo { get; set; }
+        public int? PriceFrom { get; set; }
+        public int? PriceTo { get; set; }
     }
 }
