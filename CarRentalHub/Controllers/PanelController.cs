69e53c11f448573abbfd62a01a2118d564660283
bugaj55113 <bbugi2016@gmail.com>
2024-01-16 19:21:51 +0100

    Changeable panel button - feature added.

---
+++ CarRentalHub/Controllers/PanelController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalHub.Controllers
+{
+    public class PanelController : Controller
+    {
+        public ActionResult ChangePanel(string panelType)
+        {
+            // Zapisz wybór panelu w pliku cookie
+            HttpContext.Response.Cookies.Append("PanelType", panelType);
+
+            // Możesz przekierować na dowolną akcję lub inną stronę
+            return Redirect("/Cars/MyReservations");
+        }
+    }
+}
